2abdb53fb1d6c6db5c2f30dddb2c53168b74c70d
Ubuntu <ubuntu@ip-172-31-90-217.ec2.internal>
2020-01-10 02:32:55 +0000

    changes in the nvcc md

--- power_of_two_module.py
+++ power_of_two_module.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('-p', '--raw_path')
+parser.add_argument('-s', '--size')
 args = parser.parse_args()
 
 RAW_PATH = args.raw_path
@@ -13,9 +14,10 @@ RAW_PATH = args.raw_path
 if __name__ == '__main__':
     filepaths = [os.path.join(RAW_PATH, "asphalt", i) for i in os.listdir(RAW_PATH + "/asphalt")]
 
+    size = int(args.size)
     print(filepaths)
 
     for file in filepaths:
         img = Image.open(file)
-        img = img.resize(size=(512, 512))
+        img = img.resize(size=(size, size))
         img.save(fp=file)
